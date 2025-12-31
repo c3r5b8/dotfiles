9fe87afad0748f4c0c8bee93795b871fc73ffd35
c3r5b8 <olespid@gmail.com>
2026-01-01 00:42:20 +0200

    [configs]: add fish

---
+++ configs/config.fish
@@ -0,0 +1,36 @@
+if status is-interactive
+    set -g fish_greeting
+    set -gx PATH $HOME/go/bin $PATH
+
+    alias ll='ls -lah'
+    alias gs='git status'
+    alias gco='git checkout'
+    alias vim='nvim'
+    alias top='btop'
+    alias cat='bat'
+
+    abbr gcm 'git commit -m'
+    abbr .. 'cd ..'
+    abbr ... 'cd ../..'
+    abbr update 'yay -Syu --noconfirm'
+
+    function ssh --description 'Wrap ssh with TERM=xterm-256color'
+	set -lx TERM xterm-256color
+	command ssh $argv
+    end
+
+    function starship_transient_prompt_func
+	starship module character
+    end
+    starship init fish | source
+    enable_transience
+
+    function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+	    builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+    end
+end
