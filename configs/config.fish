a86d1d57abd6dfaace4cd29595fa770962abad93
c3r5b8 <olespid@gmail.com>
2026-01-03 19:12:08 +0200

    [fish]: fix inconsistend syntax highlighting between hosts

--- configs/config.fish
+++ configs/config.fish
@@ -1,9 +1,46 @@
 if status is-interactive
-    set -g fish_greeting
     set -gx PATH $HOME/go/bin $PATH
 
     set -gx EDITOR nvim
     set -gx VISUAL nvim
+
+    set -g fish_greeting
+    set -g fish_color_autosuggestion brblack
+    set -g fish_color_cancel -r
+    set -g fish_color_command green
+    set -g fish_color_comment brblack
+    set -g fish_color_cwd green
+    set -g fish_color_cwd_root red
+    set -g fish_color_end green
+    set -g fish_color_error brred
+    set -g fish_color_escape brcyan
+    set -g fish_color_history_current --bold
+    set -g fish_color_host normal
+    set -g fish_color_host_remote yellow
+    set -g fish_color_normal normal
+    set -g fish_color_operator brcyan
+    set -g fish_color_param cyan
+    set -g fish_color_quote yellow
+    set -g fish_color_redirection cyan --bold
+    set -g fish_color_search_match white --background=brblack --bold
+    set -g fish_color_selection white --background=brblack --bold
+    set -g fish_color_status red
+    set -g fish_color_user brgreen
+    set -g fish_color_valid_path --underline
+    set -g fish_key_bindings fish_default_key_bindings
+    set -g fish_pager_color_background
+    set -g fish_pager_color_completion normal
+    set -g fish_pager_color_description yellow -i
+    set -g fish_pager_color_prefix normal --bold --underline
+    set -g fish_pager_color_progress brwhite --background=cyan --bold
+    set -g fish_pager_color_secondary_background
+    set -g fish_pager_color_secondary_completion
+    set -g fish_pager_color_secondary_description
+    set -g fish_pager_color_secondary_prefix
+    set -g fish_pager_color_selected_background -r
+    set -g fish_pager_color_selected_completion
+    set -g fish_pager_color_selected_description
+    set -g fish_pager_color_selected_prefix
 
     bind ctrl-h backward-kill-word
 
