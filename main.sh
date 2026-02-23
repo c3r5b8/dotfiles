78d2f0efc7f432553bead06e36147fe1dac3ddc6
c3r5b8 <olespid@gmail.com>
2026-02-23 15:08:14 +0200

    [script]: add ddccontrol

--- main.sh
+++ main.sh
@@ -249,6 +249,7 @@ if [[ "$HOSTNAME" == "antares" || "$HOSTNAME" == "shaula" ]]; then
 fi
 
 if [[ "$HOSTNAME" == "acrab" ]]; then
+    REQUIRED+=("ddccontrol")
     if ! echo "$RPM_JSON" | jq -r '.deployments[0]."requested-base-removals"[]?' | grep -Fxq "mesa-va-drivers"; then
         echo "Applying override: remove mesa-va-drivers, install mesa-va-drivers-freeworld"
         sudo rpm-ostree override remove mesa-va-drivers --install mesa-va-drivers-freeworld
