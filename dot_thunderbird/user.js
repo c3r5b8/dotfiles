ba17dda4ed8cebe9a37de16a5f05d724478fc835
c3r5b8 <olespid@gmail.com>
2026-09-10 22:45:27 +0300

    setup thunderbird

---
+++ dot_thunderbird/user.js
@@ -0,0 +1,161 @@
+user_pref("mail.account.account_1.identities", "id_1");
+user_pref("mail.account.account_1.server", "server_1");
+user_pref("mail.account.account_2.identities", "id_2");
+user_pref("mail.account.account_2.server", "server_2");
+user_pref("mail.account.account_3.identities", "id_3");
+user_pref("mail.account.account_3.server", "server_3");
+user_pref("mail.account.account_4.identities", "id_4");
+user_pref("mail.account.account_4.server", "server_4");
+user_pref("mail.account.account_5.identities", "id_5");
+user_pref("mail.account.account_5.server", "server_5");
+user_pref("mail.account.account_6.identities", "id_6");
+user_pref("mail.account.account_6.server", "server_6");
+
+user_pref("mail.accountmanager.accounts", "account_6,account_2,account_5,account_3,account_4,account_1,account1");
+user_pref("mail.accountmanager.defaultaccount", "account_3");
+
+user_pref("mail.identity.id_1.fullName", "Oles Pidkaura");
+user_pref("mail.identity.id_1.htmlSigText", "");
+user_pref("mail.identity.id_1.smtpServer", "smtp_1");
+user_pref("mail.identity.id_1.useremail", "o.pidkaura@colobridge.net");
+user_pref("mail.identity.id_1.valid", true);
+
+user_pref("mail.identity.id_2.fullName", "Oles Pidkaura");
+user_pref("mail.identity.id_2.htmlSigText", "");
+user_pref("mail.identity.id_2.smtpServer", "smtp_2");
+user_pref("mail.identity.id_2.useremail", "oles2005p@gmail.com");
+user_pref("mail.identity.id_2.valid", true);
+
+user_pref("mail.identity.id_3.fullName", "Oles Pidkaura");
+user_pref("mail.identity.id_3.htmlSigText", "");
+user_pref("mail.identity.id_3.smtpServer", "smtp_3");
+user_pref("mail.identity.id_3.useremail", "olespid@gmail.com");
+user_pref("mail.identity.id_3.valid", true);
+
+user_pref("mail.identity.id_4.fullName", "Oles Pidkaura");
+user_pref("mail.identity.id_4.htmlSigText", "");
+user_pref("mail.identity.id_4.smtpServer", "smtp_4");
+user_pref("mail.identity.id_4.useremail", "Pidkaura-O@st.zu.edu.ua");
+user_pref("mail.identity.id_4.valid", true);
+
+user_pref("mail.identity.id_5.fullName", "Oles Pidkaura");
+user_pref("mail.identity.id_5.htmlSigText", "");
+user_pref("mail.identity.id_5.smtpServer", "smtp_5");
+user_pref("mail.identity.id_5.useremail", "oles2005pid@gmail.com");
+user_pref("mail.identity.id_5.valid", true);
+
+user_pref("mail.identity.id_6.fullName", "Oles Pidkaura");
+user_pref("mail.identity.id_6.htmlSigText", "");
+user_pref("mail.identity.id_6.smtpServer", "smtp_6");
+user_pref("mail.identity.id_6.useremail", "c3r5b8t@gmail.com");
+user_pref("mail.identity.id_6.valid", true);
+
+user_pref("mail.openpgp.allow_external_gnupg", false);
+
+user_pref("mail.server.server_1.authMethod", 10);
+user_pref("mail.server.server_1.directory-rel", "[ProfD]ImapMail/1");
+user_pref("mail.server.server_1.hostname", "imap.gmail.com");
+user_pref("mail.server.server_1.is_gmail", true);
+user_pref("mail.server.server_1.login_at_startup", true);
+user_pref("mail.server.server_1.name", "work");
+user_pref("mail.server.server_1.port", 993);
+user_pref("mail.server.server_1.socketType", 3);
+user_pref("mail.server.server_1.type", "imap");
+user_pref("mail.server.server_1.userName", "o.pidkaura@colobridge.net");
+
+user_pref("mail.server.server_2.authMethod", 10);
+user_pref("mail.server.server_2.directory-rel", "[ProfD]ImapMail/2");
+user_pref("mail.server.server_2.hostname", "imap.gmail.com");
+user_pref("mail.server.server_2.is_gmail", true);
+user_pref("mail.server.server_2.login_at_startup", true);
+user_pref("mail.server.server_2.name", "oles2005p");
+user_pref("mail.server.server_2.port", 993);
+user_pref("mail.server.server_2.socketType", 3);
+user_pref("mail.server.server_2.type", "imap");
+user_pref("mail.server.server_2.userName", "oles2005p@gmail.com");
+
+user_pref("mail.server.server_3.authMethod", 10);
+user_pref("mail.server.server_3.directory-rel", "[ProfD]ImapMail/3");
+user_pref("mail.server.server_3.hostname", "imap.gmail.com");
+user_pref("mail.server.server_3.is_gmail", true);
+user_pref("mail.server.server_3.login_at_startup", true);
+user_pref("mail.server.server_3.name", "olespid");
+user_pref("mail.server.server_3.port", 993);
+user_pref("mail.server.server_3.socketType", 3);
+user_pref("mail.server.server_3.type", "imap");
+user_pref("mail.server.server_3.userName", "olespid@gmail.com");
+
+user_pref("mail.server.server_4.authMethod", 10);
+user_pref("mail.server.server_4.directory-rel", "[ProfD]ImapMail/4");
+user_pref("mail.server.server_4.hostname", "imap.gmail.com");
+user_pref("mail.server.server_4.is_gmail", true);
+user_pref("mail.server.server_4.login_at_startup", true);
+user_pref("mail.server.server_4.name", "university");
+user_pref("mail.server.server_4.port", 993);
+user_pref("mail.server.server_4.socketType", 3);
+user_pref("mail.server.server_4.type", "imap");
+user_pref("mail.server.server_4.userName", "Pidkaura-O@st.zu.edu.ua");
+
+user_pref("mail.server.server_5.authMethod", 10);
+user_pref("mail.server.server_5.directory-rel", "[ProfD]ImapMail/5");
+user_pref("mail.server.server_5.hostname", "imap.gmail.com");
+user_pref("mail.server.server_5.is_gmail", true);
+user_pref("mail.server.server_5.login_at_startup", true);
+user_pref("mail.server.server_5.name", "oles2005pid");
+user_pref("mail.server.server_5.port", 993);
+user_pref("mail.server.server_5.socketType", 3);
+user_pref("mail.server.server_5.type", "imap");
+user_pref("mail.server.server_5.userName", "oles2005pid@gmail.com");
+
+user_pref("mail.server.server_6.authMethod", 10);
+user_pref("mail.server.server_6.directory-rel", "[ProfD]ImapMail/6");
+user_pref("mail.server.server_6.hostname", "imap.gmail.com");
+user_pref("mail.server.server_6.is_gmail", true);
+user_pref("mail.server.server_6.login_at_startup", true);
+user_pref("mail.server.server_6.name", "c3r5b8t");
+user_pref("mail.server.server_6.port", 993);
+user_pref("mail.server.server_6.socketType", 3);
+user_pref("mail.server.server_6.type", "imap");
+user_pref("mail.server.server_6.userName", "c3r5b8t@gmail.com");
+
+user_pref("mail.smtp.defaultserver", "smtp_3");
+
+user_pref("mail.smtpserver.smtp_1.authMethod", 10);
+user_pref("mail.smtpserver.smtp_1.hostname", "smtp.gmail.com");
+user_pref("mail.smtpserver.smtp_1.port", 465);
+user_pref("mail.smtpserver.smtp_1.try_ssl", 3);
+user_pref("mail.smtpserver.smtp_1.username", "o.pidkaura@colobridge.net");
+
+user_pref("mail.smtpserver.smtp_2.authMethod", 10);
+user_pref("mail.smtpserver.smtp_2.hostname", "smtp.gmail.com");
+user_pref("mail.smtpserver.smtp_2.port", 465);
+user_pref("mail.smtpserver.smtp_2.try_ssl", 3);
+user_pref("mail.smtpserver.smtp_2.username", "oles2005p@gmail.com");
+
+user_pref("mail.smtpserver.smtp_3.authMethod", 10);
+user_pref("mail.smtpserver.smtp_3.hostname", "smtp.gmail.com");
+user_pref("mail.smtpserver.smtp_3.port", 465);
+user_pref("mail.smtpserver.smtp_3.try_ssl", 3);
+user_pref("mail.smtpserver.smtp_3.username", "olespid@gmail.com");
+
+user_pref("mail.smtpserver.smtp_4.authMethod", 10);
+user_pref("mail.smtpserver.smtp_4.hostname", "smtp.gmail.com");
+user_pref("mail.smtpserver.smtp_4.port", 465);
+user_pref("mail.smtpserver.smtp_4.try_ssl", 3);
+user_pref("mail.smtpserver.smtp_4.username", "Pidkaura-O@st.zu.edu.ua");
+
+user_pref("mail.smtpserver.smtp_5.authMethod", 10);
+user_pref("mail.smtpserver.smtp_5.hostname", "smtp.gmail.com");
+user_pref("mail.smtpserver.smtp_5.port", 465);
+user_pref("mail.smtpserver.smtp_5.try_ssl", 3);
+user_pref("mail.smtpserver.smtp_5.username", "oles2005pid@gmail.com");
+
+user_pref("mail.smtpserver.smtp_6.authMethod", 10);
+user_pref("mail.smtpserver.smtp_6.hostname", "smtp.gmail.com");
+user_pref("mail.smtpserver.smtp_6.port", 465);
+user_pref("mail.smtpserver.smtp_6.try_ssl", 3);
+user_pref("mail.smtpserver.smtp_6.username", "c3r5b8t@gmail.com");
+
+user_pref("mail.smtpservers", "smtp_6,smtp_2,smtp_5,smtp_3,smtp_4,smtp_1");
+
+
