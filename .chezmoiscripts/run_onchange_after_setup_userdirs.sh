2d48d1df5af62ae7ab621aafeb770f6a427cec92
c3r5b8 <olespid@gmail.com>
2026-09-10 22:44:56 +0300

    scripts: update userdirs script

--- .chezmoiscripts/run_onchange_after_setup_userdirs.sh
+++ .chezmoiscripts/run_onchange_after_setup_userdirs.sh
@@ -2,24 +2,21 @@
 
 # user-dirs.dirs hash: {{ include "dot_config/user-dirs.dirs" | sha256sum }}
 
-normalize_dir() {
-    local src="$1"
-    local dst="$2"
+rm_dir() {
+    local dir="$1"
 
-    if [[ -d "$src" && ! -d "$dst" ]]; then
-        mv "$src" "$dst"
+    if [[ -d "$dir" ]]; then
+        rm -rf "$dir"
     fi
-
-    mkdir -p "$dst"
 }
 
-normalize_dir "$HOME/Downloads" "$HOME/downloads"
-normalize_dir "$HOME/Documents" "$HOME/documents"
-normalize_dir "$HOME/Pictures" "$HOME/pictures"
-normalize_dir "$HOME/Music" "$HOME/music"
-normalize_dir "$HOME/Videos" "$HOME/videos"
-normalize_dir "$HOME/Desktop" "$HOME/desktop"
-normalize_dir "$HOME/Templates" "$HOME/templates"
-normalize_dir "$HOME/Public" "$HOME/public"
+rm_dir "$HOME/Downloads"
+rm_dir "$HOME/Documents"
+rm_dir "$HOME/Pictures"
+rm_dir "$HOME/Music"
+rm_dir "$HOME/Videos"
+rm_dir "$HOME/Desktop"
+rm_dir "$HOME/Templates"
+rm_dir "$HOME/Public"
 
 xdg-user-dirs-update
